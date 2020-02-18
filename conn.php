dd0cb5492ef6aca27aaa16f9d4060f3b2d9851d0
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-02-17 20:40:01 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php 
-	$conn=pg_connect("host=ec2-184-72-236-57.compute-1.amazonaws.com
+	$conn=pg_connect("host=ec2-184-72-236-57.compute-1.amazonaws.com:5432
  dbname=daa6mk2rrn3pcc user=kmazomftxczcsn password=60be9e1b1ab946560ddda8819124271491ffe193f5342277a6a8d58866b0d10b")or die("Cannnot db,please check your connection string");
 ?>
 
